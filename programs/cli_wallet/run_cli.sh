11e70f42a704e0cd4deb25cc20d34c66c802e099
Pavle Batuta <pavledb@gmail.com>
2017-01-16 12:40:54 +0100

    Removed chain id from run cli

--- programs/cli_wallet/run_cli.sh
+++ programs/cli_wallet/run_cli.sh
@@ -3,6 +3,5 @@
 WALLET_NAME="wallet.json"
 NODE_WS_PORT="9881"
 NODE_WS_ADDRESS="ws://localhost:$NODE_WS_PORT"
-CHAIN_ID="e6f9f44e4404287c6a4b6da1db76b1fee28572590d882041f33ee02c1932dfdb"
 
-./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS" --chain-id "$CHAIN_ID"
+./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS"

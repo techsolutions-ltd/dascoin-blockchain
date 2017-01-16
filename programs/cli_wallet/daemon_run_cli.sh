60b6650d3b1046b731f08502ad5fa828eb8bfeb7
Pavle Batuta <pavledb@gmail.com>
2017-01-16 12:39:05 +0100

    Removed chain-id from daemon cli script

--- programs/cli_wallet/daemon_run_cli.sh
+++ programs/cli_wallet/daemon_run_cli.sh
@@ -3,6 +3,5 @@
 WALLET_NAME="wallet.json"
 NODE_WS_PORT="9880"
 NODE_WS_ADDRESS="ws://localhost:$NODE_WS_PORT"
-CHAIN_ID="b282ab4e8a3272b191f2747d4c480732da4b07fbddbae6f9dda5ffed73c3a58e"
 
-./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS" --chain-id "$CHAIN_ID" -r 0.0.0.0:6000 -d
+./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS" -r 0.0.0.0:6000 -d

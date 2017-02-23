1f195f9004eca7ca6aeb38e957137b878ae67673
Pavle Batuta <pavledb@gmail.com>
2017-02-23 12:37:29 +0100

    New fast testing configuration
    * Delays for license requests, issue requests reduced to 1s

---
+++ configs/testing_fast/cli_wallet/daemon_run_cli.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+WALLET_NAME="wallet.json"
+NODE_WS_PORT="9880"
+CLI_WALLET_PORT="6000"
+NODE_WS_ADDRESS="ws://localhost:$NODE_WS_PORT"
+CLI_WALLET_ADDRESS="0.0.0.0:6000"
+
+./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS" -r "$CLI_WALLET_ADDRESS" -d
---
+++ configs/testing_fast/cli_wallet/run_cli.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+WALLET_NAME="wallet.json"
+NODE_WS_PORT="9880"
+NODE_WS_ADDRESS="ws://localhost:$NODE_WS_PORT"
+
+./cli_wallet --wallet "$WALLET_NAME" -s "$NODE_WS_ADDRESS"

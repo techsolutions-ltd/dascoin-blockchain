f8520a69b8be246a83aee3d827663ed0198391a3
Damir <damir@roundglobe.tech>
2017-09-21 12:57:54 +0200

    Do not use now(), instead use head_block_time when setting next reset time. Fixes EP-65.

--- libraries/chain/db_update.cpp
+++ libraries/chain/db_update.cpp
@@ -520,7 +520,7 @@ void database::reset_spending_limits()
     // Set the time of the next limit reset:
     modify(dgpo, [&](dynamic_global_property_object& dgpo){
       dgpo.last_daily_dascoin_price = dgpo.last_dascoin_price;
-      uint32_t now_sec = fc::time_point::now().sec_since_epoch();
+      uint32_t now_sec = head_block_time().sec_since_epoch();
       uint32_t next_interval = (now_sec / params.limit_interval_elapse_time_seconds) *
                                 params.limit_interval_elapse_time_seconds + params.limit_interval_elapse_time_seconds;
       // Move to the next interval if the current calculation returns the same point as the previous one:

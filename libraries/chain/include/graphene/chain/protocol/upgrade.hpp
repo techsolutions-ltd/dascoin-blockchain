9d27328db7d18dde66b357213ea544078734ec91
Damir <damir@roundglobe.tech>
2017-11-08 09:31:10 +0100

    Fix indent.

--- libraries/chain/include/graphene/chain/protocol/upgrade.hpp
+++ libraries/chain/include/graphene/chain/protocol/upgrade.hpp
@@ -6,35 +6,36 @@
 
 namespace graphene { namespace chain {
 
-    struct create_upgrade_event_operation : public base_operation
-    {
-      struct fee_parameters_type {};
-      asset fee;   // always zero
+  struct create_upgrade_event_operation : public base_operation
+  {
+    struct fee_parameters_type {};
+    asset fee;   // always zero
 
-      account_id_type upgrade_creator;
-      time_point_sec execution_time;
-      optional<time_point_sec> cutoff_time;
-      vector<time_point_sec> subsequent_execution_times;
-      string comment;
+    account_id_type upgrade_creator;
+    time_point_sec execution_time;
+    optional<time_point_sec> cutoff_time;
+    vector<time_point_sec> subsequent_execution_times;
+    string comment;
 
-      extensions_type extensions;
+    extensions_type extensions;
 
-      create_upgrade_event_operation() = default;
-      explicit create_upgrade_event_operation(account_id_type upgrade_creator,
-                                              time_point_sec execution_time,
-                                              optional<time_point_sec> cutoff_time,
-                                              vector<time_point_sec> subsequent_execution_times,
-                                              string comment)
-        : upgrade_creator(upgrade_creator),
-          execution_time(execution_time),
-          cutoff_time(cutoff_time),
-          subsequent_execution_times(std::move(subsequent_execution_times)),
-          comment(std::move(comment)) { }
+    create_upgrade_event_operation() = default;
+    explicit create_upgrade_event_operation(account_id_type upgrade_creator,
+                                            time_point_sec execution_time,
+                                            optional<time_point_sec> cutoff_time,
+                                            vector<time_point_sec> subsequent_execution_times,
+                                            string comment)
+      : upgrade_creator(upgrade_creator),
+        execution_time(execution_time),
+        cutoff_time(cutoff_time),
+        subsequent_execution_times(std::move(subsequent_execution_times)),
+        comment(std::move(comment)) { }
 
-      account_id_type fee_payer() const { return upgrade_creator; }
-      void validate() const;
-      share_type calculate_fee(const fee_parameters_type& k) const { return 0; }
-    };
+    account_id_type fee_payer() const { return upgrade_creator; }
+    void validate() const;
+    share_type calculate_fee(const fee_parameters_type& k) const { return 0; }
+  };
+
 } }  // namespace graphene::chain
 
 ////////////////////////////////

1b318740416fa65cc145d8773a56690ca1011627
Damir <damir@roundglobe.tech>
2018-01-24 11:01:26 +0100

    Make api-access reading more robust.

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -393,11 +393,21 @@ namespace detail {
             _force_validate = true;
          }
 
-         graphene::time::now();
+         if( _options->count("api-access") ) {
 
-         if( _options->count("api-access") )
-            _apiaccess = fc::json::from_file( _options->at("api-access").as<boost::filesystem::path>() )
-               .as<api_access>();
+            if(fc::exists(_options->at("api-access").as<boost::filesystem::path>()))
+            {
+              _apiaccess = fc::json::from_file( _options->at("api-access").as<boost::filesystem::path>() ).as<api_access>();
+              ilog( "Using api access file from ${path}",
+                    ("path", _options->at("api-access").as<boost::filesystem::path>().string()) );
+            }
+            else
+            {
+              elog("Failed to load file from ${path}",
+                 ("path", _options->at("api-access").as<boost::filesystem::path>().string()));
+              std::exit(EXIT_FAILURE);
+            }
+         }
          else
          {
             // TODO:  Remove this generous default access policy

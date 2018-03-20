3b89e2c8507476bff40f8fea399c541fd9c9efeb
Damir <damir@roundglobe.tech>
2018-03-20 12:19:07 +0100

    Make sure that calculated number of cycles matches expected amount.

--- libraries/chain/protocol/cycle.cpp
+++ libraries/chain/protocol/cycle.cpp
@@ -65,9 +65,8 @@ void purchase_cycle_asset_operation::validate() const
   FC_ASSERT( frequency > 0 );
   FC_ASSERT( expected_amount > 0 );
   // Make sure we get an integer value of cycles:
-  auto cycles_to_receive = (amount * frequency) % (DASCOIN_DEFAULT_ASSET_PRECISION * DASCOIN_FREQUENCY_PRECISION);
-  FC_ASSERT( cycles_to_receive == 0 );
-  FC_ASSERT( cycles_to_receive == expected_amount );
+  FC_ASSERT( (amount * frequency) % (DASCOIN_DEFAULT_ASSET_PRECISION * DASCOIN_FREQUENCY_PRECISION) == 0 );
+  FC_ASSERT( (amount * frequency) / (DASCOIN_DEFAULT_ASSET_PRECISION * DASCOIN_FREQUENCY_PRECISION) == expected_amount );
 }
 
 } } // namespace graphene::chain
